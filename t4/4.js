5af1f4e873f08f6b21bba337090580fa76bbc99e
Alexander Wolff <awfinland@gmail.com>
2024-11-25 02:41:34 +0200

    t4 push

--- t4/4.js
+++ t4/4.js
@@ -13,3 +13,12 @@ const students = [
     id: '5423679',
   },
 ];
+
+const targetElement = document.getElementById("target");
+let listItem = "";
+for(let i = 0; i < students.length; i++) {
+  const item = document.createElement("option");
+  item.value = students[i].id;
+  item.textContent = students[i].name;
+  targetElement.appendChild(item);
+}
