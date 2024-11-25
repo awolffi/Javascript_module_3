bcd829d3660c168022134404e80e1c26518b0ee2
Alexander Wolff <awfinland@gmail.com>
2024-11-25 02:04:03 +0200

    t3 push

--- t3/3.js
+++ t3/3.js
@@ -1,2 +1,10 @@
 'use strict';
 const names = ['John', 'Paul', 'Jones'];
+const targetElement = document.getElementById("target");
+let listOfStuff = "";
+
+for (let i = 0; i<names.length; i++) {
+    listOfStuff += `<li>${names[i]}</li>`;
+}
+
+targetElement.innerHTML = listOfStuff;
